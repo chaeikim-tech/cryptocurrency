20d2322d6a4401fc5af63bf7a81fbdeea7cda5b8
eunju.kim <sb02348@hanmail.net>
2022-01-08 19:04:24 +0900

    styled-component example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Father = styled.div`
+  display:block;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`;
+
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Father as="header">
+      <Box bgColor="teal"/>
+      <Circle bgColor="tomato" />
+      <Btn>Log in</Btn>
+      <Btn as="a" href="/">Log in</Btn>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+    </Father>
+    
   );
 }
 
