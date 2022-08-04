715075222cfc9bcb2c1a3e90e035c6e7178185f8
chaeikim <sb02348@hanmail.net>
2022-08-04 16:38:52 +0900

    Add back button

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,11 +1,18 @@
-import { useQuery } from 'react-query';
-import { Outlet, Link, useLocation, useParams, useMatch} from 'react-router-dom';
-import styled from 'styled-components';
-import { fetchCoinInfo, fetchCoinTickers } from '../api';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { useQuery } from "react-query";
+import {
+  Outlet,
+  Link,
+  useLocation,
+  useParams,
+  useMatch,
+} from "react-router-dom";
+import styled from "styled-components";
+import { fetchCoinInfo, fetchCoinTickers } from "../api";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const Title = styled.h1`
   font-size: 48px;
+  margin-left: -55px;
   color: ${(props) => props.theme.accentColor};
 `;
 
@@ -23,16 +30,16 @@ const Container = styled.div`
 const Header = styled.header`
   height: 15vh;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
 `;
 
 const Overview = styled.div`
-    display: flex;
-    justify-content: space-between;
-    background-color: rgba(0, 0, 0, 0.5);
-    padding: 10px 20px;
-    border-radius: 10px;
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
 `;
 
 const OverviewItem = styled.div`
@@ -69,148 +76,169 @@ const Tab = styled.span<{ isActive: boolean }>`
   color: ${(props) =>
     props.isActive ? props.theme.accentColor : props.theme.textColor};
   a {
-    padding:7px 0px;
+    padding: 7px 0px;
     display: block;
   }
 `;
 
+const BackButton = styled.button`
+  padding: 4px 8px;
+  border-radius: 8px;
+  font-size: 1.5rem;
+  line-height: 1.2;
+  border: 3px solid ${(props) => props.theme.textColor};
+  color: ${(props) => props.theme.bgColor};
+  background: ${(props) => props.theme.accentColor};
+  cursor: pointer;
+`;
+
 interface RouteParams {
-    [index: string]: string
-    coinId: string;
-  };
+  [index: string]: string;
+  coinId: string;
+}
 
 interface RouteState {
-    state:{
-        name: string;
-    }
+  state: {
+    name: string;
   };
+}
 
 interface InfoData {
-    id: string;
-    name: string;
-    symbol: string;
-    rank: number;
-    is_new: boolean;
-    is_active: boolean;
-    type: string;
-    description: string;
-    message: string;
-    open_source: boolean;
-    started_at: string;
-    development_status: string;
-    hardware_wallet: boolean;
-    proof_type: string;
-    org_structure: string;
-    hash_algorithm: string;
-    first_data_at: string;
-    last_data_at: string;
-  }
-  
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
 interface PriceData {
-    id: string;
-    name: string;
-    symbol: string;
-    rank: number;
-    circulating_supply: number;
-    total_supply: number;
-    max_supply: number;
-    beta_value: number;
-    first_data_at: string;
-    last_updated: string;
-    quotes: {
-      USD: {
-        ath_date: string;
-        ath_price: number;
-        market_cap: number;
-        market_cap_change_24h: number;
-        percent_change_1h: number;
-        percent_change_1y: number;
-        percent_change_6h: number;
-        percent_change_7d: number;
-        percent_change_12h: number;
-        percent_change_15m: number;
-        percent_change_24h: number;
-        percent_change_30d: number;
-        percent_change_30m: number;
-        percent_from_price_ath: number;
-        price: number;
-        volume_24h: number;
-        volume_24h_change_24h: number;
-      };
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
     };
-  }
-
- 
-
+  };
+}
 
 function Coin() {
-    const { coinId } = useParams<RouteParams>();
-    const { state } = useLocation() as RouteState;
-    const priceMatch = useMatch("/:coinId/price");
-    const chartMatch = useMatch("/:coinId/chart");
-    const { isLoading: infoLoading, data: infoData} = useQuery<InfoData>(
-        ["info", coinId], () => fetchCoinInfo(coinId!)
-        //https://stackoverflow.com/questions/54496398/typescript-type-string-undefined-is-not-assignable-to-type-string/54496418
-        //(coinId"!")=> 확장 할당 어션셜로 값이 무조건 할당되어있다고 컴파일러에게 전달해 값이 없어도 변수를 사용할 수 있게 함
-    );
+  const { coinId } = useParams<RouteParams>();
+  const { state } = useLocation() as RouteState;
+  const priceMatch = useMatch("/:coinId/price");
+  const chartMatch = useMatch("/:coinId/chart");
+  const { isLoading: infoLoading, data: infoData } = useQuery<InfoData>(
+    ["info", coinId],
+    () => fetchCoinInfo(coinId!)
+    //https://stackoverflow.com/questions/54496398/typescript-type-string-undefined-is-not-assignable-to-type-string/54496418
+    //(coinId"!")=> 확장 할당 어션셜로 값이 무조건 할당되어있다고 컴파일러에게 전달해 값이 없어도 변수를 사용할 수 있게 함
+  );
 
-    const { isLoading: tickersLoading, data: tickersData } = useQuery<PriceData>(
-        ["tickers", coinId], () => fetchCoinTickers(coinId!), { refetchInterval: 5000,}
-    );
-    const loading = infoLoading || tickersLoading;
+  const { isLoading: tickersLoading, data: tickersData } = useQuery<PriceData>(
+    ["tickers", coinId],
+    () => fetchCoinTickers(coinId!),
+    { refetchInterval: 5000 }
+  );
+  const loading = infoLoading || tickersLoading;
 
-    return (
-        <Container>
-            <HelmetProvider>
-              <Helmet>
-                <title>
-                    {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
-                </title>
-              </Helmet>
-            </HelmetProvider>
-            <Header>
-                <Title>{state?.name ? state.name : loading ? "Loading..." : infoData?.name }</Title>
-            </Header>
-            {loading ? <Loader>Loading...</Loader> : (
-                <>
-                <Overview>
-                    <OverviewItem>
-                        <span>Rank:</span>
-                        <span>{infoData?.rank}</span>
-                    </OverviewItem>
-                    <OverviewItem>
-                        <span>Symbol:</span>
-                        <span>${infoData?.symbol}</span>
-                    </OverviewItem>
-                    <OverviewItem>
-                        <span>Price:</span>
-                        <span>{tickersData?.quotes?.USD?.price?.toFixed(3)}</span>
-                    </OverviewItem>
-                </Overview>
-                <Description>{infoData?.description}</Description>
-                <Overview>
-                  <OverviewItem>
-                    <span>Total Supply:</span>
-                    <span>${tickersData?.total_supply}</span>
-                  </OverviewItem>
-                  <OverviewItem>
-                    <span>Max Supply:</span>
-                    <span>{tickersData?.max_supply}</span>
-                  </OverviewItem>
-                </Overview>
-                <Tabs>
-                    <Tab isActive={chartMatch !== null}>
-                        <Link to={`/${coinId}/chart`}>Chart</Link>
-                    </Tab>
-                    <Tab isActive={priceMatch !== null}>
-                        <Link to={`/${coinId}/price`}>Price</Link>
-                    </Tab>
-                </Tabs>
-                <Outlet />
-                </>
-            ) }
-        </Container>
-    );
+  return (
+    <Container>
+      <HelmetProvider>
+        <Helmet>
+          <title>
+            {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
+          </title>
+        </Helmet>
+      </HelmetProvider>
+      <Header>
+        <Link to="/">
+          <BackButton type="button" onClick={() => console.log("back")}>
+            ⬅
+          </BackButton>
+        </Link>
+        <Title>
+          {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
+        </Title>
+        <div></div>
+      </Header>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{infoData?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${infoData?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Price:</span>
+              <span>{tickersData?.quotes?.USD?.price?.toFixed(3)}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{infoData?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Supply:</span>
+              <span>${tickersData?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{tickersData?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          <Tabs>
+            <Tab isActive={chartMatch !== null}>
+              <Link to={`/${coinId}/chart`}>Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !== null}>
+              <Link to={`/${coinId}/price`}>Price</Link>
+            </Tab>
+          </Tabs>
+          <Outlet />
+        </>
+      )}
+    </Container>
+  );
 }
 
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,100 +1,118 @@
-import { useQuery } from 'react-query';
+import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { fetchCoins } from '../api';
-import { useSetRecoilState } from 'recoil';
-import { isDarkAtom } from '../atom';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { fetchCoins } from "../api";
+import { useSetRecoilState } from "recoil";
+import { isDarkAtom } from "../atom";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const Container = styled.div`
-    padding: 0px 20px;
-    max-width: 500px;
-    margin: 0 auto;
+  padding: 0px 20px;
+  max-width: 500px;
+  margin: 0 auto;
 `;
 
 const Header = styled.header`
-    height: 15vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  height: 15vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const CoinsList = styled.ul``;
 
 const Coin = styled.li`
-    background-color: ${(props) => props.theme.cardBgColor};
-    color: ${(props) => props.theme.textColor};
-    border-radius: 15px;
-    margin-bottom: 10px;
-    border: 1px solid white;
+  background-color: ${(props) => props.theme.cardBgColor};
+  color: ${(props) => props.theme.textColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+  border: 1px solid white;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    transition: color 0.2s ease-in;
+  }
+  &:hover {
     a {
-        display: flex;
-        align-items: center;
-        padding: 20px;
-        transition: color 0.2s ease-in;
+      color: ${(props) => props.theme.accentColor};
     }
-    &:hover {
-        a {
-            color: ${(props) => props.theme.accentColor};
-        }
-    }
+  }
 `;
 
 const Title = styled.h1`
-    font-size: 48px;
-    color: ${(props) => props.theme.accentColor};
+  font-size: 48px;
+  margin-right: -55px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const ToggleButton = styled.button`
+  padding: 4px 8px;
+  border-radius: 8px;
+  font-size: 1rem;
+  font-weight: 600;
+  line-height: 1.2;
+  border: 3px solid ${(props) => props.theme.bgColor};
+  color: ${(props) => props.theme.textColor};
+  background: ${(props) => props.theme.accentColor};
+  cursor: pointer;
 `;
 
 const Loader = styled.span`
-    text-align: center;
-    display: block;
+  text-align: center;
+  display: block;
 `;
 
 const Img = styled.img`
-    width: 35px;
-    height: 35px;
-    margin-right: 10px;
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
 `;
 
 interface ICoin {
-    id: string;
-    name: string;
-    symbol: string;
-    rank: number;
-    is_new: boolean;
-    is_active: boolean;
-    type: string;
-  }
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 
 function Coins() {
-    const setDarkAtom = useSetRecoilState(isDarkAtom);
-    const toggleDarkAtom = () => setDarkAtom((prev) => !prev);
-    const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
-    return (
-        <Container>
-            <HelmetProvider>
-                <Helmet>
-                    <title>Coin</title>
-                </Helmet>
-            </HelmetProvider>
-            <Header>
-                <Title>Coins</Title>
-                <button onClick={toggleDarkAtom}>Toggle Mode</button>
-            </Header>
-            {isLoading ? (<Loader>Loading...</Loader>) : (
-                <CoinsList>
-                {data?.slice(0,100).map((coin) => (
-                  <Coin key={coin.id}>
-                    <Link to={`/${coin.id}`} state={coin.name}>
-                      <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}/>
-                      {coin.name} &rarr;
-                    </Link>
-                  </Coin>
-                ))}
-              </CoinsList>
-            )}
-        </Container>
-    )
+  const setDarkAtom = useSetRecoilState(isDarkAtom);
+  const toggleDarkAtom = () => setDarkAtom((prev) => !prev);
+  const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
+  return (
+    <Container>
+      <HelmetProvider>
+        <Helmet>
+          <title>Coin</title>
+        </Helmet>
+      </HelmetProvider>
+      <Header>
+        <div></div>
+        <Title>Coins</Title>
+        <ToggleButton onClick={toggleDarkAtom}>Toggle Mode</ToggleButton>
+      </Header>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinsList>
+          {data?.slice(0, 100).map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`/${coin.id}`} state={coin.name}>
+                <Img
+                  src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                />
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
+    </Container>
+  );
 }
 
 export default Coins;
